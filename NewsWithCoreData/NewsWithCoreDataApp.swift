1393656ba3c302d0cab970fa344b47c8fd38b8e6
Artem Bolotov <teeemon@gmail.com>
2023-03-23 08:42:49 +0300

    Initial Commit

---
+++ NewsWithCoreData/NewsWithCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsWithCoreDataApp.swift
+//  NewsWithCoreData
+//
+//  Created by artembolotov on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsWithCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
